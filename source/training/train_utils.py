c49a86af0373b8cd505ed9e3ea40f4c20f894f99
raflidzaky <raflidzakyansyah@gmail.com>
2025-01-29 06:40:49 +0700

    Define classes to build train pipeline

--- source/training/train_utils.py
+++ source/training/train_utils.py
@@ -3,6 +3,8 @@ import yaml
 import pandas as pd
 import os
 from pydantic import BaseModel
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 # Create config loader from config yaml
 class ConfigLoader:
@@ -37,6 +39,12 @@ class DataLoader:
         dataset = pd.read_csv(self.file_path)
         return dataset
     
+    def load_target(self, dataset):
+        target = self.config_loader.get('target', {})
+        target_column = list(target.values())
+        data_Y = dataset[target_column]
+        return data_Y
+
     def load_feature(self, dataset):
         # Parse the feature needed
         features = self.config_loader.get('feature', {})
@@ -45,11 +53,40 @@ class DataLoader:
         feature_columns = list(features.values()) 
 
         # And pass it here
-        data_fixture = dataset[feature_columns]
-        return data_fixture
+        data_X = dataset[feature_columns]
+        return data_X
     
-# Def the splitting
+    def train_split(self, X, y):
+        test_size = self.config_loader.get('data_split', {}).get('test_size')  # Default to 0.1 if not found
+        random_state = self.config_loader.get('data_split', {}).get('random_state') 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, 
+                                                            random_state=random_state)
+        return X_train, X_test, y_train, y_test
 
 # Def the model selection (model name and params)
+class ModelLoader:
+    def __init__(self, config_loader):
+        self.config_loader = config_loader
+    
+    def load_model(self):
+        # Load model type and parameters from the YAML config
+        model_config = self.config_loader.get('model', {})
+        model_type = model_config.get('type')  # Default to DecisionTreeClassifier if not found
+        params = model_config.get('params', {})
 
+        # Instantiate the model based on the model type
+        if model_type == "DecisionTreeClassifier":
+            model = DecisionTreeClassifier(**params)
+        else:
+            raise ValueError(f"Model type '{model_type}' is not supported.")
+        return model
+    
 # Def the model training
+class ModelTrainer:
+    def __init__(self, model_loader):
+        self.config_loader = model_loader
+    
+    def train_model(self, X_train, y_train):
+        model = self.model_loader.load_model()
+        model.fit(X_train, y_train)
+        return model
