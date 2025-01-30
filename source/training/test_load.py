1e1c6e3ec7f32f906f6b16888f785e79aafb89a7
raflidzaky <raflidzakyansyah@gmail.com>
2025-01-30 13:31:45 +0700

    Set up fixtures for all test cases

--- source/training/test_load.py
+++ source/training/test_load.py
@@ -2,11 +2,66 @@ from train_utils import ConfigLoader, DataLoader, ModelLoader, ModelTrainer
 import numpy as np
 import pytest
 
-# TODO: Apply fixture decorators on each test case
-# Since the data is not from a static data (rather a "dynamic" one like model loader)
-# Fixture making sure to set up the data and "freeze it"
-# Thus, inject the data to each test case
+# Fixtures for data and model setup
+# The core ideas of fixture:
+# 1. Fixture making sure to set up the data and "freeze it". Thus inject the data to every test case
+# 2. However, since ML testing must be interdependent (making sure the flow logic is correctly done)
+# The data has to be set up once for ALL OVER the test
+# Thus, we need "scope" parameters in the decorators
+# 3. If the scope is being deleted, for each every tests, pytest will load the data. 
+# This will make the test operation heavy. Unless we want to test with fresh data for every test cases.
 
+@pytest.fixture(scope='module')
+def config_loader():
+    config_file = 'C:/Users/Rafli/decision-pursue-mba/config.yaml'
+    return ConfigLoader(config_file=config_file)
+
+@pytest.fixture(scope='module')
+def model_loader(config_loader):
+    return ModelLoader(config_loader=config_loader)
+
+@pytest.fixture(scope='module')
+def data_loader(config_loader):
+    data_file = 'C:/Users/Rafli/decision-pursue-mba/source/training/data/mba_decision_dataset.csv'
+    return DataLoader(file_path=data_file, config_loader=config_loader)
+
+@pytest.fixture(scope='module')
+def dataset(data_loader):
+    return data_loader.load_data()
+
+@pytest.fixture(scope='module')
+def X(data_loader, dataset):
+    return data_loader.load_feature(dataset=dataset)
+
+@pytest.fixture(scope='module')
+def y(data_loader, dataset):
+    return data_loader.load_target(dataset=dataset)
+
+@pytest.fixture(scope='module')
+def X_train(X, y, data_loader):
+    X_train, X_test, y_train, y_test = data_loader.train_split(X=X, y=y)
+    return X_train
+
+@pytest.fixture(scope='module')
+def X_test(X, y, data_loader):
+    X_train, X_test, y_train, y_test = data_loader.train_split(X=X, y=y)
+    return X_test
+
+@pytest.fixture(scope='module')
+def y_train(X, y, data_loader):
+    X_train, X_test, y_train, y_test = data_loader.train_split(X=X, y=y)
+    return y_train
+
+@pytest.fixture(scope='module')
+def y_test(X, y, data_loader):
+    X_train, X_test, y_train, y_test = data_loader.train_split(X=X, y=y)
+    return y_test
+
+@pytest.fixture(scope='module')
+def model(model_loader):
+    return model_loader.load_model()
+
+# Define the test cases
 def test_X_type(X):
     # Test that X is a NumPy array
     assert isinstance(X, np.ndarray), f"Expected X to be a numpy.ndarray, but got {type(X)}"
@@ -56,31 +111,3 @@ def test_prediction(model):
     dummy=[[0.9, 0.9]]
     prediction = model.predict(dummy)
     assert prediction == "Yes"
-
-# Initialize config path
-config_file = 'C:/Users/Rafli/decision-pursue-mba/config.yaml'
-data_file = 'C:/Users/Rafli/decision-pursue-mba/source/training/data/mba_decision_dataset.csv' 
-
-# Start to config data and model
-config_loader = ConfigLoader(config_file=config_file)
-model_loader  = ModelLoader(config_loader=config_loader)
-data_loader   = DataLoader(file_path=data_file, 
-                           config_loader=config_loader)
-
-dataset = data_loader.load_data()
-model   = model_loader.load_model()
-X = data_loader.load_feature(dataset=dataset)
-y = data_loader.load_target(dataset=dataset)
-
-X_train, X_test, y_train, y_test = data_loader.train_split(X=X, 
-                                                           y=y)
-    
-test_X_type(X=X)
-test_X_element_type(X=X)
-test_y_type(y=y)
-test_split(X=X, X_train=X_train, X_test=X_test,
-           y=y, y_train=y_train, y_test=y_test)
-test_fitting(model=model, 
-             X_train=X_train, 
-             y_train=y_train)
-test_prediction(model=model)
